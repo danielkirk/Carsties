fb5cb28b5feffad9480b63e78f2f02f567929055
Daniel Kirk <imdanielkirk@gmail.com>
2024-05-07 20:55:38 -0700

    added seeding for postgres and not sqllite for identity

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+325335790d85ee71be7f2c12c1fa2cebf04b2d5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae2c57a3f8d23e948e675b19ad16455cfd1ff9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
